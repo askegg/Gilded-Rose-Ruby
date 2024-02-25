eb0807186fe978104eb40f36e562f4d8be35b248
Andrew Skegg <andrew.skegg@icloud.com>
2024-02-25 10:55:11 +1000

    Move the case statement into a hash
    Now we look up the name in the hash and return the appropriate value. If we do not have a match, we will simply return the default Item class.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -2,24 +2,17 @@ require "zeitwerk"
 loader = Zeitwerk::Loader.for_gem(warn_on_extra_files: false)
 loader.setup
 
+DEFAULT_CLASS = Item
+SPECIALISED_CLASSES = {
+  "Normal Item" => Item::Normal,
+  "Aged Brie" => Item::Brie,
+  "Backstage passes to a TAFKAL80ETC concert" => Item::Backstage,
+  "Conjured Mana Cake" => Item::Conjured
+}
+
 module GildedRose
   def self.new(name:, days_remaining:, quality:)
-    klass_name = klass_for(name).to_s
+    klass_name = (SPECIALISED_CLASSES[name] || DEFAULT_CLASS).to_s
     Kernel.const_get(klass_name).new(days_remaining: days_remaining, quality: quality)
-  end
-
-  def self.klass_for(name)
-    case name
-    when "Aged Brie"
-      Item::Brie
-    when "Sulfuras, Hand of Ragnaros"
-      Item
-    when "Backstage passes to a TAFKAL80ETC concert"
-      Item::Backstage
-    when "Conjured Mana Cake"
-      Item::Conjured
-    else
-    Item::Normal
-    end
   end
 end
