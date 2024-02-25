4766e87bb4423e25643dc3c68cc8d2301613fd65
Andrew Skegg <andrew.skegg@icloud.com>
2024-02-25 11:37:08 +1000

    Move configuration into YAML file

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -2,17 +2,12 @@ require "zeitwerk"
 loader = Zeitwerk::Loader.for_gem(warn_on_extra_files: false)
 loader.setup
 
-DEFAULT_CLASS = Item
-SPECIALISED_CLASSES = {
-  "Normal Item" => Item::Normal,
-  "Aged Brie" => Item::Brie,
-  "Backstage passes to a TAFKAL80ETC concert" => Item::Backstage,
-  "Conjured Mana Cake" => Item::Conjured
-}
+require "yaml"
+ITEM_TYPES = YAML.load_file("config/items.yml")
 
 module GildedRose
   def self.new(name:, days_remaining:, quality:)
-    klass_name = (SPECIALISED_CLASSES[name] || DEFAULT_CLASS).to_s
+    klass_name = ITEM_TYPES["SPECIALISED_CLASSES"][name] || ITEM_TYPES["DEFAULT_CLASS"]
     Kernel.const_get(klass_name).new(days_remaining: days_remaining, quality: quality)
   end
 end
