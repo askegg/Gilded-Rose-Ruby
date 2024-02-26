fdd1ed776f5bee353f5afae6efed08be3ba24feb
Andrew Skegg <andrew.skegg@icloud.com>
2024-02-26 13:04:14 +1000

    Made item class names more readable.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -7,7 +7,7 @@ ITEM_TYPES = YAML.load_file("config/items.yml")
 
 module GildedRose
   def self.new(name:, days_remaining:, quality:)
-    klass_name = ITEM_TYPES["SPECIALISED_CLASSES"][name] || ITEM_TYPES["DEFAULT_CLASS"]
+    klass_name = ITEM_TYPES["ITEMS"][name] || ITEM_TYPES["DEFAULT"]
     Kernel.const_get(klass_name).new(days_remaining: days_remaining, quality: quality)
   end
 end
