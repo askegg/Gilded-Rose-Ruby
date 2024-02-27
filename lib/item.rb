c24c8435b6a988cff34384c53c888ac5c2c5a9a3
Andrew Skegg <andrew.skegg@icloud.com>
2024-02-28 06:50:59 +1000

    Refactor item.rb
    More graceful handling of options.

--- lib/item.rb
+++ lib/item.rb
@@ -1,9 +1,13 @@
 class Item
   attr_reader :days_remaining, :quality
 
-  def initialize(options = {})
-    @days_remaining = options[:days_remaining] || 0
-    @quality = options[:quality] || 0
+  def initialize(**args)
+    defaults = {days_remaining: 0, quality: 0}
+    args.compact! # Remove nils
+    options = defaults.merge(args)
+
+    @days_remaining = options[:days_remaining]
+    @quality = options[:quality]
   end
 
   def tick
