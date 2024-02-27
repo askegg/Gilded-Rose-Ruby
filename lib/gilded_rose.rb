658e34dfcbaf5b9653a0c367ca1b31535f06a59e
Andrew Skegg <andrew.skegg@icloud.com>
2024-02-28 09:36:22 +1000

    Refactored GildedRose
    ITEM_TYPES now frozen for security.
    Switched to double splat for arguments instead of options hash.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -3,16 +3,16 @@ loader = Zeitwerk::Loader.for_gem(warn_on_extra_files: false)
 loader.setup
 
 require "yaml"
-ITEM_TYPES = YAML.load_file("config/items.yml")
+ITEM_TYPES = YAML.load_file("config/items.yml").freeze
 
 module GildedRose
-  def self.new(options = {})
-    klass_name = ITEM_TYPES["ITEMS"][options[:name]] || ITEM_TYPES["DEFAULT"]
+  def self.new(**args)
+    klass_name = ITEM_TYPES["ITEMS"][args[:name]] || ITEM_TYPES["DEFAULT"]
 
     Kernel.const_get(klass_name)
       .new(
-        days_remaining: options[:days_remaining],
-        quality: options[:quality]
+        days_remaining: args[:days_remaining],
+        quality: args[:quality]
       )
   end
 end
