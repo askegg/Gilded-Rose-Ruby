e699cbbef56398f1742bc3b8b563bbab49b6c5b1
Andrew Skegg <andrew.skegg@icloud.com>
2024-02-27 14:56:45 +1000

    Make it just that little bit more readable

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,8 +8,11 @@ ITEM_TYPES = YAML.load_file("config/items.yml")
 module GildedRose
   def self.new(options = {})
     klass_name = ITEM_TYPES["ITEMS"][options[:name]] || ITEM_TYPES["DEFAULT"]
+
     Kernel.const_get(klass_name)
-      .new(days_remaining: options[:days_remaining],
-        quality: options[:quality])
+      .new(
+        days_remaining: options[:days_remaining],
+        quality: options[:quality]
+      )
   end
 end
