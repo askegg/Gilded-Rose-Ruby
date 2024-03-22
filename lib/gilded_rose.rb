6c99c0b2579957437e41a76a16bd5e428cb6537b
Andrew Skegg <andrew.skegg@icloud.com>
2024-03-22 12:13:39 +1000

    Moved init to run.rb

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,3 +1,7 @@
+require "yaml"
+app_root = File.expand_path("../", __dir__)
+ITEM_TYPES = YAML.load_file(File.new("#{app_root}/config/items.yml")).freeze
+
 module GildedRose
   def self.new(**args)
     klass_name = ITEM_TYPES["ITEMS"][args[:name]] || ITEM_TYPES["DEFAULT"]
--- lib/run.rb
+++ lib/run.rb
@@ -3,8 +3,4 @@ loader = Zeitwerk::Loader.new
 loader.push_dir(File.dirname(__FILE__))
 loader.setup
 
-require "yaml"
-app_root = File.expand_path("../", __dir__)
-ITEM_TYPES = YAML.load_file(File.new("#{app_root}/config/items.yml")).freeze
-
-puts GildedRose.new
+puts GildedRose.new(name: "Conjured Mana Cake")
